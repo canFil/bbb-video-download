b2971a365dc8c4bbcf789c7636e3bf8351150152
Can Fil <gfcan85@gmail.com>
2020-12-05 16:01:22 +0300

    Added vimeo upload.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { ArgumentParser } = require('argparse')
 const { version } = require('./package.json')
 const { Processor } = require('./modules/processor')
+const client_id = 'fb7697803f42ba95ad8473f86143c435f5222808';
+const client_secret = 'F/4l/gaN4g6FI8HWrFVqa4OwUq5gHen0BTsE5Lo2fEF6W66HB1zr/iQCvK9RgO6tGcCDCv08eQVj95x6PQZmO4Tikul1k1J7SQamTrHW/RpuIXAlio/+LCdTm26cS1cM';
+const access_token = '5394b25527d3c4dd6858294d4237798e';
+var path = require('path');
+let Vimeo = require('vimeo').Vimeo;
+let client = new Vimeo(`${client_id}`, `${client_secret}`, `${access_token}`);
 
 const argsParser = new ArgumentParser()
 argsParser.add_argument('-v', '--version', {
@@ -59,7 +65,34 @@ async function process(args) {
         console.error(error)
         console.log(error)
     }
+
+    try {
+        uploadFile(args.output)
+    } catch (error) {
+        console.error(error)
+        console.log(error)
+    }
 }
+
+async function uploadFile(file_name) {
+    client.upload(
+        file_name,
+        {
+          'name': path.basename(file_name), //'Untitled',
+          'description': path.basename(file_name) //'The description goes here.'
+        },
+        async function (uri) {
+          console.log('Your video URI is: ' + uri);
+        },
+        function (bytes_uploaded, bytes_total) {
+          var percentage = (bytes_uploaded / bytes_total * 100).toFixed(2)
+          console.log(bytes_uploaded, bytes_total, percentage + '%')
+        },
+        function (error) {
+          console.log('Failed because: ' + error)
+        }
+      )
+} 
 
 process(args)
 
